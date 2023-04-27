096ef42f878a41ab57e907560c188129ee46ec50
Nguyen Tran <nguyentran1072e@gmail.com>
2023-04-27 15:34:02 -0400

    Lesson 2: Hello world kernel!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello world!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
   loop {}
@@ -10,5 +12,16 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+  // Pointer to VGA buffer
+  let vga_buffer = 0xb8000 as *mut u8;
+
+  // Iterate over Hello world!
+  for (i, &byte) in HELLO.iter().enumerate() {
+    unsafe {
+      // Think in C: *(vga_buffer + 2 * i) or *(vga_buffer + 2 * i + 1)
+      *vga_buffer.offset(2 * i as isize) = byte; // character
+      *vga_buffer.offset(2 * i as isize + 1) = 0xb; // cyan colour
+    }
+  }
   loop {}
 }
