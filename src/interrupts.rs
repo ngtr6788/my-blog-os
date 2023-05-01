ec679e826b6f3915343aea1285ea6574fdfb6d06
Nguyen Tran <nguyentran1072e@gmail.com>
2023-05-01 12:38:31 -0400

    Lesson 5: Set up the double fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,11 +1,17 @@
+// Sets up exception and interrupt handlers
+
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
+use crate::{println, gdt};
 
 lazy_static! {
   static ref IDT: InterruptDescriptorTable = {
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpoint_handler);
+    unsafe {
+      idt.double_fault.set_handler_fn(double_fault_handler)
+        .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+    }
     idt
   };
 }
@@ -16,6 +22,10 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stackframe: InterruptStackFrame) {
   println!("EXCEPTION: BREAKPOINT\n{:#?}", stackframe);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stackframe: InterruptStackFrame, _error_code: u64) -> ! {
+  panic!("EXCEPTION: DOUBLE_FAULT\n{:#?}", stackframe);
 }
 
 #[test_case]
