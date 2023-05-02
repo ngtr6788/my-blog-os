44aeaebf2e23240a2b9a0e3f9e8b564f16f6255a
Nguyen Tran <nguyentran1072e@gmail.com>
2023-05-02 16:04:38 -0400

    Lesson 6: Keyboard device interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -5,7 +5,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::{print, println, gdt};
 use pic8259::ChainedPics;
 use spin::Mutex;
-
+use pc_keyboard::{Keyboard, layouts::Us104Key, ScancodeSet1, HandleControl, DecodedKey};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -15,7 +15,8 @@ pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(PIC_1
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum InterruptIndex {
-  Timer = PIC_1_OFFSET
+  Timer = PIC_1_OFFSET,
+  Keyboard,
 }
 
 impl InterruptIndex {
@@ -33,11 +34,19 @@ lazy_static! {
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpoint_handler);
     idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_hanlder);
+    idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
     unsafe {
       idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
     }
     idt
   };
+}
+
+lazy_static! {
+  static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> = 
+    Mutex::new(
+      Keyboard::new(Us104Key, ScancodeSet1, HandleControl::Ignore)
+    );
 }
 
 pub fn init_idt() {
@@ -56,6 +65,27 @@ extern "x86-interrupt" fn timer_interrupt_hanlder(_stackframe: InterruptStackFra
   print!(".");
   unsafe {
     PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+  };
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stackframe: InterruptStackFrame) {
+  use x86_64::instructions::port::Port;
+  
+  let mut ps2_data_port = Port::new(0x60);
+  let mut keyboard = KEYBOARD.lock();
+  let scancode: u8 = unsafe { ps2_data_port.read() };
+
+  if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+    if let Some(key) = keyboard.process_keyevent(key_event) {
+      match key {
+        DecodedKey::Unicode(character) => print!("{}", character),
+        DecodedKey::RawKey(key) => print!("{:?}", key)
+      }
+    }
+  }
+
+  unsafe {
+    PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
   };
 }
 
